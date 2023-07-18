2593609805c48856e8c14b514c403ac83dc066e9
Nilmar Ramos <nilmar@imaginationmedia.com>
2023-07-18 17:05:24 -0300

    add all

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
-import Sidebar from "@/components/Sidebar";
 import Card from "@/components/Card";
+import Sidebar from "@/components/Sidebar";
+import TopCard from "@/components/TopCard";
 
 export default function Home() {
   return (
     <main className="flex gap-16">
       <Sidebar />
-      <Card />
+      <div>
+        <TopCard />
+        <div className="flex gap-6">
+          <Card
+            title="Despesas por categoria"
+            action={{ title: "Ver mais", link: "/despesas" }}
+          >
+            <div className="h-96">
+              <div className="border-"></div>
+            </div>
+          </Card>
+          <Card
+            title="Receitas por categoria"
+            action={{ title: "Ver mais", link: "/receitas" }}
+          >
+            <div className="h-96"></div>
+          </Card>
+        </div>
+      </div>
     </main>
   );
 }
