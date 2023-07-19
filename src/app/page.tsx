87c3ea2844728721ae616c1e37eb7387801282e6
Nilmar Ramos <nilmar@imaginationmedia.com>
2023-07-19 00:11:41 -0300

    add charts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+"use client";
 import Card from "@/components/Card";
 import Sidebar from "@/components/Sidebar";
 import TopCard from "@/components/TopCard";
+import dynamic from "next/dynamic";
+import type { ApexOptions } from "apexcharts";
+
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 export default function Home() {
+  const options: ApexOptions = {
+    chart: {
+      type: "donut",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    legend: {
+      show: false,
+    },
+  };
+
   return (
     <main className="flex gap-16">
       <Sidebar />
@@ -13,15 +30,17 @@ export default function Home() {
             title="Despesas por categoria"
             action={{ title: "Ver mais", link: "/despesas" }}
           >
-            <div className="h-96">
-              <div className="border-"></div>
+            <div className="h-96 flex justify-center items-center">
+              <Chart options={options} series={[44, 55]} type="donut" />
             </div>
           </Card>
           <Card
             title="Receitas por categoria"
             action={{ title: "Ver mais", link: "/receitas" }}
           >
-            <div className="h-96"></div>
+            <div className="h-96 flex justify-center items-center">
+              <Chart options={options} series={[70, 91, 125]} type="donut" />
+            </div>
           </Card>
         </div>
       </div>
